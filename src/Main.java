a7e72785a15dcebe8d1fc0c1e2edd63e5d3949e8
Eka Starostina <starostinaeka@gmail.com>
2023-01-22 17:17:45 +0300

    add changes

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int mile = 20;
+        int ticketPrice = 500;
+
+        int x
+
+        if (ticketPrice / mile) {
+        System.out.println("Total Miles");
+        System.out.println(x);
+
+    }
+}
